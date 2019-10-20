c9d3f5f1fd3574d65235520f080ebcbf4299a30d
Eric Gao <754095810@qq.com>
2019-10-20 18:13:35 +0800

    new homework

---
+++ git-example/src/main/java/com/example/Hello.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Hello {
+    public void hello() {}
+
+    public void bug() {
+        int num = 10;
+        int a = num + 10;
+    }
+    public void feature() {
+        System.out.println("Hello world! ");
+    }
+    public void fixBug() {}
+}
